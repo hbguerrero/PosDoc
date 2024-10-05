e3cfcef63ec9f1887bfa40ebc3d0f6629cfcb0e9
hbguerrero <141659945+hbguerrero@users.noreply.github.com>
2024-10-04 20:02:48 -0500

    Update arduino.c

--- serial1/arduino.c
+++ serial1/arduino.c
@@ -2,10 +2,18 @@ char cmd;                     //Define a variável dos comandos seriais
 
 void setup() {
   Serial.begin(9600);         //Inicia o Monitor Serial
-  pinMode(15, OUTPUT);         //Define o pino como saída
   pinMode(14, OUTPUT);         //Define o pino como saída
-  digitalWrite(14, LOW);
-  digitalWrite(15, LOW);
+  pinMode(15, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(18, OUTPUT);         //Define o pino como saída
+  pinMode(19, OUTPUT);
+  digitalWrite(14, 0);
+  digitalWrite(15, 0);
+  digitalWrite(2, 0);
+  digitalWrite(4, 0);
+  digitalWrite(18, 0);
+  digitalWrite(19, 0);
 }
 
 void loop() {
@@ -13,15 +21,36 @@ void loop() {
   if (cmd == 'w') {           //Se o comando for "l", liga o led
     analogWrite(14, 0);
     analogWrite(15, 0);
+    analogWrite(2, 0);
+    analogWrite(4, 64);
+    analogWrite(18, 64);
+    analogWrite(19, 0);
   }
 
   else if (cmd == 'd') {      //Se o comando for "d", desliga o led
     analogWrite(14, 0);
-    analogWrite(15, 128);
+    analogWrite(15, 32);
+    analogWrite(2, 0);
+    analogWrite(4, 64);
+    analogWrite(18, 0);
+    analogWrite(19, 64);
   }
 
   else if (cmd == 'a') {      //Se o comando for "d", desliga o led
-    analogWrite(14, 128);
-    analogWrite(15, LOW);
+    analogWrite(14, 32);
+    analogWrite(15, 0);
+    analogWrite(2, 0);
+    analogWrite(4, 64);
+    analogWrite(18, 0);
+    analogWrite(19, 64);
+  }
+
+  else if (cmd == 'p') {      //Se o comando for "d", desliga o led
+    analogWrite(14, 0);
+    analogWrite(15, 0);
+    analogWrite(2, 0);
+    analogWrite(4, 0);
+    analogWrite(18, 0);
+    analogWrite(19, 0);
   }
 }
