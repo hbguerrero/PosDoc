69017eeea300b7ba657bc84de4a2b010cefdfe02
hbguerrero <141659945+hbguerrero@users.noreply.github.com>
2025-05-21 19:09:27 -0500

    Update main.cpp

--- Ros2YDlidar_and_microRos_BNO055_platformio_graciasIvan/main.cpp
+++ Ros2YDlidar_and_microRos_BNO055_platformio_graciasIvan/main.cpp
@@ -26,13 +26,32 @@ rcl_timer_t timer;
 
 Adafruit_BNO055 bno = Adafruit_BNO055(55);
 
-unsigned long lastTime = 0;
 float ImuIni;
 
 #define LED_PIN 2
-#define L_PIN 32
-#define R_PIN 33
+const int C1 = 32; // Entrada de la señal A del encoder.
+const int C2 = 33; // Entrada de la señal B del encoder.
+const int PWM1 = 13;
+const int PWM2 = 12;
+const int MOTOR0 = 18; //IZQUIERDA
+const int MOTOR1 = 19; //DERECHA
 
+volatile int  n    = 0;
+volatile byte ant  = 0;
+volatile byte act  = 0;
+
+unsigned long lastTime = 0;  // Tiempo anterior
+unsigned long sampleTime = 10;  // Tiempo de muestreo
+
+double P = 0;
+double R = 35020;   //Parece que ppr = 11; reducción = 515; precición cuádrupla =>  R = 11*4*515
+//double R = 22660;   //Parece que ppr = 11; reducción = 515; precición cuádrupla =>  R = 11*4*515
+//double R = 1460;   //Parece que ppr = 11; reducción = 515; precición cuádrupla =>  R = 11*4*515
+double d = 0;
+double pwm =0;
+int pwm0 = 0;
+
+void encoder(void);
 
 // subscriber
 rcl_subscription_t subscriber;
@@ -75,36 +94,83 @@ void subscription_callback(const void *msgin)
 
   //digitalWrite(L_PIN, (msg_led->data == 0) ? LOW : HIGH); 
 
-  if ((msg_led -> data < 2) and (msg_led -> data > -2) ) {
-    digitalWrite(L_PIN, HIGH);
-    digitalWrite(R_PIN, HIGH);
-  } 
-  else if ((msg_led -> data < -2)) {
-    digitalWrite(L_PIN, HIGH);
-    digitalWrite(R_PIN, LOW);
-  }  
+  if (millis() - lastTime >= sampleTime || lastTime==0)
+  {  // Se actualiza cada sampleTime (milisegundos)
+      lastTime = millis();
+      P = (n*360.0)/R;
+   }
+  
+  // VALORES NEGATIVOS GIRA DERECHA, VALORS POSITIVOS GIRA IZQUIERDA
+  if ((msg_led->data <= 45.0) and (msg_led->data >=-45.0)) {    
+    if (P > msg_led->data){
+      analogWrite(PWM1, 0); //retrocede o derecha
+      analogWrite(PWM2, 64);
+      delay(40);
+      analogWrite(PWM1, 0);
+      analogWrite(PWM2, 0);                            
+     
+      if(P >= -2 && P <= 2) { 
+      analogWrite(MOTOR1, 128);
+      analogWrite(MOTOR0, 128);
+      }
+      if(P < -2) { 
+      analogWrite(MOTOR1, 64);
+      analogWrite(MOTOR0, 128);
+      }
+      if(P > 2) { 
+      analogWrite(MOTOR1, 128);
+      analogWrite(MOTOR0, 64);
+      }     
+     }
+     
+    else if (P<=msg_led->data){
+      if (P>msg_led->data - 0.7 ){
+        analogWrite(PWM1, 0); //para
+        analogWrite(PWM2, 0);
+      }
+      else{        
+        analogWrite(PWM1, 64);  //avance o izquierda
+        analogWrite(PWM2, 0);
+        delay(40);
+        analogWrite(PWM1, 0);
+        analogWrite(PWM2, 0);        
+      }
 
+      if(P >= -2 && P <= 2) { 
+      analogWrite(MOTOR1, 128);
+      analogWrite(MOTOR0, 128);
+      }
+      if(P < -2) { 
+      analogWrite(MOTOR1, 64);
+      analogWrite(MOTOR0, 128);
+      }
+      if(P > 2) { 
+      analogWrite(MOTOR1, 128);
+      analogWrite(MOTOR0, 64);
+      }      
+    }
+  
   else {
-    digitalWrite(L_PIN, LOW);
-    digitalWrite(R_PIN, HIGH);
-  }  
-
-  // Process message
-  //printf("Received: %d\n", msg_led->data);
+    analogWrite(PWM1, 0);
+    analogWrite(PWM2, 0);
+  } 
+  }
 }
-
 
 void setup() {
   Serial.begin(115200);
   set_microros_serial_transports(Serial);
-  
-  pinMode(LED_PIN, OUTPUT);
-  pinMode(L_PIN, OUTPUT);
-  pinMode(R_PIN, OUTPUT);
 
-  digitalWrite(LED_PIN, HIGH);
-  digitalWrite(L_PIN, LOW);
-  digitalWrite(R_PIN, LOW);  
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(C1, INPUT);
+  pinMode(C2, INPUT);
+  pinMode(PWM1, OUTPUT);
+  pinMode(PWM2, OUTPUT);
+  pinMode(MOTOR0, OUTPUT);
+  pinMode(MOTOR1, OUTPUT);
+
+  attachInterrupt(digitalPinToInterrupt(C1), encoder, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(C2), encoder, CHANGE);  
   
   if(!bno.begin())
   {
@@ -162,4 +228,29 @@ void loop() {
   //RCSOFTCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100)));
   RCCHECK(rclc_executor_spin_some(&executor_pub, RCL_MS_TO_NS(100)));
   RCCHECK(rclc_executor_spin_some(&executor_sub, RCL_MS_TO_NS(100)));
+}
+
+void encoder(void)
+{
+
+  ant=act;
+  
+  if(digitalRead(C1)) bitSet(act,1); else bitClear(act,1);            
+  if(digitalRead(C2)) bitSet(act,0); else bitClear(act,0);
+
+    if(n < R) {
+    if(ant == 2 && act ==0) n++;
+    if(ant == 0 && act ==1) n++;
+    if(ant == 3 && act ==2) n++;
+    if(ant == 1 && act ==3) n++;
+  }
+  else {n = 0;}
+
+  if(n > -R){
+    if(ant == 1 && act ==0) n--;
+  if(ant == 3 && act ==1) n--;
+  if(ant == 0 && act ==2) n--;
+  if(ant == 2 && act ==3) n--;    
+  }
+  else {n = 0;}      
 }
