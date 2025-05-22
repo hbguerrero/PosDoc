18eb9dee4aac205db2cd68d08ad39768435cb1bb
hbguerrero <141659945+hbguerrero@users.noreply.github.com>
2025-05-21 19:09:05 -0500

    Update publisher_member_fuction.py

--- Ros2YDlidar_and_microRos_BNO055_platformio_graciasIvan/lidarFuentes/publisher_member_fuction.py
+++ Ros2YDlidar_and_microRos_BNO055_platformio_graciasIvan/lidarFuentes/publisher_member_fuction.py
@@ -1,124 +1,42 @@
+# Recibe el dato del bno (subscriptor), calcula error de orientación (eYaw)
+# y lo publica en un tópico que llamamos u
+
+
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import String, Int32, Float32
 
-import os
-import ydlidar
+from std_msgs.msg import String, Float32, Int32
 
-import time
-import math
+global bno
+bno = 9.9
 
-import csv
-
-
-ydlidar.os_init()
-ports = ydlidar.lidarPortList()
-port = "/dev/ydlidar"
-
-lx = 0
-sumatoria = 0
-promedio = 0
-contador = 0
-BWHP = 2
-DEG2RAD = 0.0174533
-PIMED = 1.5708
-
-for key, value in ports.items():
-    port = value
-laser = ydlidar.CYdLidar()
-laser.setlidaropt(ydlidar.LidarPropSerialPort, port)
-laser.setlidaropt(ydlidar.LidarPropSerialBaudrate, 128000)
-laser.setlidaropt(ydlidar.LidarPropLidarType, ydlidar.TYPE_TRIANGLE)
-laser.setlidaropt(ydlidar.LidarPropDeviceType, ydlidar.YDLIDAR_TYPE_SERIAL)
-laser.setlidaropt(ydlidar.LidarPropScanFrequency, 10.0)
-laser.setlidaropt(ydlidar.LidarPropSampleRate, 9)
-laser.setlidaropt(ydlidar.LidarPropSingleChannel, True)
-
-
-class MinimalPublisher(Node):
+class MinimalSubscriber(Node):
 
     def __init__(self):
-        super().__init__('minimal_publisher')
+        super().__init__('minimal_subscriber')
+        self.publisher_ = self.create_publisher(Float32, 'u', 10)
+        self.subscription = self.create_subscription(Float32, 'lidar', self.listener_callback, 10)
+        self.subscription  # prevent unused variable warning
 
-        self.subscription = self.create_subscription(Float32, 'bno', self.listener_callback, 10)
-        self.publisher_ = self.create_publisher(Float32, 'lidar', 10)
-
-        """
-        timer_period = 0.5  # seconds
-        self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.i = 0.0
-        """
-        
     def listener_callback(self, msg):
-
-        ret = laser.initialize()
-
-        if ret:
-            ret = laser.turnOn()
-            scan = ydlidar.LaserScan()
-            if ret and ydlidar.os_isOk():
-                r = laser.doProcessSimple(scan)
-                sumatoria = 0
-                contador = 0.0
-                promedio = 0.0
-
-                if msg.data == 360.0:
-                    self.rads = 0.0
-                else:
-                    self.rads = (msg.data)*math.pi/180
-
-                if 0 <= self.rads and self.rads < math.pi/2:
-                    self.eYaw = 0.0 - self.rads
-                elif self.rads < 2*(math.pi) and self.rads > 3*(math.pi/2):
-                    self.eYaw = 2*(math.pi) - self.rads            
-
-                if r:
-                    with open('LidarReadings.csv', 'a') as csvfile:
-                        #writer = csv.writer(csvfile)
-                        #row = []
-                        # print("Scan received[",scan.stamp,"]:",scan.points.size(),"ranges is [",1.0/scan.config.scan_time,"]Hz");
-                        for point in scan.points:
-                            if point.angle < ((PIMED + ((BWHP*DEG2RAD)/2)) + self.eYaw) and (point.angle > ((PIMED - ((BWHP*DEG2RAD)/2)) + self.eYaw)):
-                            #reading = ""
-                              if point.range > 0.0:
-                                if (point.range > 0) and (point.range < 1.0):
-                                    #lx = point.range*math.cos((point.angle - math.pi/2) + self.eYaw)  ## Aqui NO disloca el cono, solo usu eYaw para la proyección
-                                    lx = point.range*math.cos(point.angle  - (math.pi/2 + self.eYaw))  ## Aqui SI disloca el cono, solo usu eYaw para la proyección
-                                    sumatoria = sumatoria + lx
-                                    contador = contador + 1
-                                    self.i = sumatoria/contador
-                            #self.i = 0.0
-                            #reading += str(point.range)+"@"+str(point.angle)
-                            #row.append(reading)
-                        #writer.writerow(row)
-                                    #print(promedio)
-                #                    print("angle:", point.angle, " range: ", point.range)#
-                #                    print("angle:", point.angle, " lx: ", lx)
-                                    # print("angle:", point.angle, " range: ", lx)
-                #                   time.sleep(1)
-                        msg.data = self.i
-                        #print(self.i, self.eYaw)
-                        self.publisher_.publish(msg)
-                else:
-                    print("Failed to get Lidar Data")
-                time.sleep(0.05)
-
+        #self.get_logger().info("%f" % msg.data)
+                    
+        if ((msg.data > 0.4) and (msg.data < 0.5)):
+           preU = 0.0
+        elif (msg.data < 0.4):
+           preU = -30.0
+        else:
+            preU = 30.0
+#       u = (msg.data)*2.0
+        msg.data = preU
+        self.publisher_.publish(msg)
 
 def main(args=None):
     rclpy.init(args=args)
-
-    minimal_publisher = MinimalPublisher()
-
-    rclpy.spin(minimal_publisher)
-
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
-    minimal_publisher.destroy_node()
+    node = MinimalSubscriber()
+    rclpy.spin(node)
+    #minimal_subscriber.destroy_node()
     rclpy.shutdown()
-    laser.turnOff()
-    laser.disconnecting()
-
 
 if __name__ == '__main__':
     main()
